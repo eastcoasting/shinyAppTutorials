901769ee3ba0b862f9efef7a7e86a82d514ee759
eastcoasting <62911962+eastcoasting@users.noreply.github.com>
2020-06-25 17:27:51 -0400

    Update tileImages.js

--- Internal-Links-Basic-Ex/www/js/tileImages.js
+++ Internal-Links-Basic-Ex/www/js/tileImages.js
@@ -5,5 +5,5 @@
 ////////////////////////////////////////////////////////////////////////////////
 // BEGIN
 $( document ).ready(function() {
-    $( ".navbar .container-fluid" ).append('<a href="https://www.conservation.org/"><img src=\"CI.png\" alt=\"alt\" style=\"float:right;width:156.88px;height:60px;padding-top:5px;padding-bottom:5px; margin:2px;\"> </a>', '<a href="https://www.iucn.org/"><img src=\"IUCN.png\" alt=\"alt\" style=\"float:right;width:51.92px;height:60px;padding-top:5px;padding-bottom:5px;\"> </a>');
+    $( ".navbar .container-fluid" ).append('<a href="https://www.conservation.org/" target="_blank"><img src=\"CI.png\" alt=\"alt\" style=\"float:right;width:156.88px;height:60px;padding-top:5px;padding-bottom:5px; margin-left:20px;\"> </a>', '<a href="https://www.iucn.org/" target="_blank"><img src=\"IUCN.png\" alt=\"alt\" style=\"float:right;width:51.92px;height:60px;padding-top:5px;padding-bottom:5px;\"> </a>');
 });
